db55c6f941706ab4bd3fc6fc02eda6042af2159c
IvanR96 <irios96@hotmail.com>
2025-10-18 19:00:42 -0700

    set up complete

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-let userIsAuthorized = false;
-
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.get("/", (req, res) => {
+let userIsAuthorised = false;
+
+function passwordCheck(req, res, next){
+    let password = req.body.password;
+
+    if(password === "tellmesecrets"){
+        userIsAuthorised = true;
+    }
+    next();
+};
+
+app.use(passwordCheck);
+
+app.get("/", (req,res) =>{
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/check", (req, res,) =>{
-    const password = req.body["password"];
-    if (password === "ILoveProgramming"){
-        userIsAuthorized = true;
+app.post("/check", (req, res) =>{
+    if(userIsAuthorised){
         res.sendFile(__dirname + "/secret.html");
     }
     else{
@@ -26,9 +35,10 @@ app.post("/check", (req, res,) =>{
     }
 });
 
-app.listen(port,( req, res)=>{
-    console.log(`Server running on port ${port}.`);
-});
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}...`);
+}); 
+
 
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
